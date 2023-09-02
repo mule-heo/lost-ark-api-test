d9ea5618ac3c6cd4c91fd3b4695e9b1957aaf165
mule-heo <devheojinwoo@gmail.com>
2023-09-02 17:47:08 +0900

    Initial commit

---
+++ client/src/app/index.tsx
@@ -0,0 +1,16 @@
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <header></header>
+      <section>
+        <aside></aside>
+        <main></main>
+      </section>
+      <footer></footer>
+    </>
+  );
+}
+
+export default App;
