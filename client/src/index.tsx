aa3f3507ee12a0da7c7c408146f48f18b9ad3068
mule-heo <devheojinwoo@gmail.com>
2023-09-03 00:00:24 +0900

    Add react-query provider

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,12 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "app/providers/main-router";
+import { QueryClientProvider } from "react-query";
+import { queryClient } from "app/providers/query-client";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={true} />
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
