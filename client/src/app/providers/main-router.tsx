4ab812e4310ec5ee665c3fc7c3e1d1b31e285b20
mule-heo <devheojinwoo@gmail.com>
2023-09-05 00:59:25 +0900

    refactor nested router

--- client/src/app/providers/main-router.tsx
+++ client/src/app/providers/main-router.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
-import Root from "app/index";
-import ErrorPage from "pages/error-page";
+import Root from "app";
+import { ErrorPage, MainPage, ApiPage } from "pages";
 
 export const router = createBrowserRouter([
   {
@@ -9,32 +9,43 @@ export const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [
       {
-        path: "api/news",
-        element: <div>news</div>,
+        index: true,
+        element: <MainPage />,
       },
       {
-        path: "api/characters",
-        element: <div>characters</div>,
-      },
-      {
-        path: "api/armories",
-        element: <div>armories</div>,
-      },
-      {
-        path: "api/auctions",
-        element: <div>auctions</div>,
-      },
-      {
-        path: "api/guilds",
-        element: <div>guilds</div>,
-      },
-      {
-        path: "api/markets",
-        element: <div>markets</div>,
-      },
-      {
-        path: "api/gamecontents",
-        element: <div>gamecontents</div>,
+        path: "/api",
+        element: <ApiPage />,
+        children: [
+          {
+            path: "api/news",
+            element: <div>news</div>,
+            index: true,
+          },
+          {
+            path: "api/characters",
+            element: <div>characters</div>,
+          },
+          {
+            path: "api/armories",
+            element: <div>armories</div>,
+          },
+          {
+            path: "api/auctions",
+            element: <div>auctions</div>,
+          },
+          {
+            path: "api/guilds",
+            element: <div>guilds</div>,
+          },
+          {
+            path: "api/markets",
+            element: <div>markets</div>,
+          },
+          {
+            path: "api/gamecontents",
+            element: <div>gamecontents</div>,
+          },
+        ],
       },
     ],
   },
