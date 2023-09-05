b9587b9864e149017857e3f5257dff6cb9a4db83
Jinwoo Heo <83588163+mule-heo@users.noreply.github.com>
2023-09-05 22:52:39 +0900

    Refactor: remove unnecessary routes (#20)

--- client/src/app/providers/main-router.tsx
+++ client/src/app/providers/main-router.tsx
@@ -15,37 +15,6 @@ export const router = createBrowserRouter([
       {
         path: "/api",
         element: <ApiPage />,
-        children: [
-          {
-            path: "api/news",
-            element: <div>news</div>,
-            index: true,
-          },
-          {
-            path: "api/characters",
-            element: <div>characters</div>,
-          },
-          {
-            path: "api/armories",
-            element: <div>armories</div>,
-          },
-          {
-            path: "api/auctions",
-            element: <div>auctions</div>,
-          },
-          {
-            path: "api/guilds",
-            element: <div>guilds</div>,
-          },
-          {
-            path: "api/markets",
-            element: <div>markets</div>,
-          },
-          {
-            path: "api/gamecontents",
-            element: <div>gamecontents</div>,
-          },
-        ],
       },
     ],
   },
