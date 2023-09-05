114a60b6c71138ab553f1cb49f1813aa0b4582d2
mule-heo <devheojinwoo@gmail.com>
2023-09-05 22:39:11 +0900

    add sidebar to api-page

--- client/src/pages/api-page/api-page.tsx
+++ client/src/pages/api-page/api-page.tsx
@@ -1,8 +1,20 @@
+import { Sidebar } from "shared/ui";
+import { useState } from "react";
+
 export const ApiPage = () => {
+  // TODO: location 접근해서 초깃값 설정하는 게 side effect 있는지 확인하고 수정할 것
+  const [currentCategory, setCurrentCategory] = useState<string>(
+    location.href
+      .replace(location.origin + "/api", "")
+      .replace("/", "")
+      .toUpperCase() || "NEWS",
+  );
   return (
     <>
-      <aside></aside>
-      <main></main>
+      <Sidebar
+        currentCategory={currentCategory}
+        setCurrentCategory={setCurrentCategory}
+      />
     </>
   );
 };
