6210e25dd21fce31d94ea049f486ce5303684b65
mule-heo <devheojinwoo@gmail.com>
2023-09-14 03:42:13 +0900

    Move addQueryParameters function

---
+++ client/src/shared/util/params/params.ts
@@ -0,0 +1,15 @@
+export const addQueryParameters = (
+  url: string,
+  parameters: { [key: string]: string | number },
+) => {
+  let count = 0;
+  for (const key of Object.keys(parameters)) {
+    if (count === 0) {
+      url += `?${key}=${parameters[key]}`;
+    } else {
+      url += `&${key}=${parameters[key]}`;
+    }
+    count++;
+  }
+  return url;
+};
