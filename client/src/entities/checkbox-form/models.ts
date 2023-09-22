3d56e7e86ba39019710dbfecf3fa3d48a1b21483
mule-heo <devheojinwoo@gmail.com>
2023-09-23 01:48:46 +0900

    add Filters interface

---
+++ client/src/entities/checkbox-form/models.ts
@@ -0,0 +1,11 @@
+export interface Filters {
+  profiles: boolean;
+  equipment: boolean;
+  avatars: boolean;
+  "combat-skills": boolean;
+  engravings: boolean;
+  cards: boolean;
+  gems: boolean;
+  colosseums: boolean;
+  collectibles: boolean;
+}
