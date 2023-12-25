dc9d9491b4d0d60040f99d67f31fecbeecc41f3a
mule-heo <devheojinwoo@gmail.com>
2023-12-26 05:11:28 +0900

    add api tag and paragraph

--- client/src/entities/accordion/accordion.tsx
+++ client/src/entities/accordion/accordion.tsx
@@ -1,9 +1,11 @@
 import { useRef } from "react";
 import * as S from "./style";
-import { Parameter } from "pages/api-page/endpoints";
+import { Parameter, ApiMetadata } from "pages/api-page/endpoints";
 import { RequestForm } from "entities/request-form";
+import { ApiTag } from "shared/ui/apiTag";
 
 export const Accordion = (props: {
+  apiMetadata: ApiMetadata;
   requestKey: string;
   requestFn: () => void;
   parameters: Parameter[];
@@ -23,7 +25,13 @@ export const Accordion = (props: {
 
   return (
     <S.DivContainer>
-      <button onClick={handleClickAccordion}>열기</button>
+      <S.Summary onClick={handleClickAccordion}>
+        <ApiTag theme={props.apiMetadata.method}>
+          {props.apiMetadata.method}
+        </ApiTag>
+        <S.P>{props.apiMetadata.endpoint}</S.P>
+        <S.P>{props.apiMetadata.description}</S.P>
+      </S.Summary>
       <S.DivHidden ref={divRef}>
         <RequestForm {...props} />
       </S.DivHidden>
