92bbeb4a496373cf1f09aa525bb73be7afb9df9c
mule-heo <devheojinwoo@gmail.com>
2024-01-01 23:01:47 +0900

    prevent execution of useEffect when first rendered

--- client/src/features/window/scroll.ts
+++ client/src/features/window/scroll.ts
@@ -1,4 +1,4 @@
-import { RefObject, useEffect } from "react";
+import { RefObject, useEffect, useRef } from "react";
 
 export const useScroll = (dependencies: (number | string | boolean)[]) => {
   useEffect(() => {
@@ -10,10 +10,46 @@ export const useResizeAccordion = (
   divRef: RefObject<HTMLDivElement>,
   dependencies: (number | string | boolean)[],
 ) => {
+  const firstExecution = useRef(true);
+
   useEffect(() => {
-    if (divRef.current) {
-      const height = divRef.current.scrollHeight;
-      divRef.current.style.maxHeight = `${Math.ceil(height)}px`;
+    if (firstExecution.current === true) {
+      firstExecution.current = false;
+      return;
+    } else {
+      if (divRef.current) {
+        const height = divRef.current.scrollHeight;
+        divRef.current.style.maxHeight = `${Math.ceil(height)}px`;
+      }
     }
   }, [...dependencies]);
 };
+
+export const resizeAccordion = (divRef: RefObject<HTMLDivElement>) => {
+  if (divRef.current) {
+    const height = divRef.current.scrollHeight;
+    divRef.current.style.maxHeight = `${Math.ceil(height)}px`;
+  }
+};
+
+// TODO: 확장성 개선하기
+// https://stackoverflow.com/questions/53179075/with-useeffect-how-can-i-skip-applying-an-effect-upon-the-initial-render
+export function useDidUpdateEffect(
+  fn: (divRef: RefObject<HTMLDivElement>) => void,
+  divRef: RefObject<HTMLDivElement>,
+  dependencies: (number | string | boolean)[],
+) {
+  const isMountingRef = useRef(false);
+
+  useEffect(() => {
+    isMountingRef.current = true;
+  }, []);
+
+  useEffect(() => {
+    if (!isMountingRef.current) {
+      return fn(divRef);
+    } else {
+      isMountingRef.current = false;
+    }
+  }, dependencies);
+}
