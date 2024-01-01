ab0a098fd1e0da83a3134c86ed13c300d26d0521
mule-heo <devheojinwoo@gmail.com>
2024-01-01 19:07:32 +0900

    add accordion resize hook

--- client/src/features/window/scroll.ts
+++ client/src/features/window/scroll.ts
@@ -1,7 +1,19 @@
-import { useEffect } from "react";
+import { RefObject, useEffect } from "react";
 
 export const useScroll = (dependencies: (number | string | boolean)[]) => {
   useEffect(() => {
     window.scrollTo(0, 0);
+  }, [...dependencies]);
+};
+
+export const useResizeAccordion = (
+  divRef: RefObject<HTMLDivElement>,
+  dependencies: (number | string | boolean)[],
+) => {
+  useEffect(() => {
+    if (divRef.current) {
+      const height = divRef.current.scrollHeight;
+      divRef.current.style.maxHeight = `${Math.ceil(height)}px`;
+    }
   }, [...dependencies]);
 };
